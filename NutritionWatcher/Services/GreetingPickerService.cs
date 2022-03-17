f52344efce3a04eedb7a40a1a69fde6144803194
PurpleAnteater <I.Kupchinenko@gmail.com>
2022-03-18 01:20:24 +0300

    Added service for creating greeting depending on current time

---
+++ NutritionWatcher/Services/GreetingPickerService.cs
@@ -0,0 +1,35 @@
+﻿namespace NutritionWatcher.Services
+{
+    public class GreetingPickerService
+    {
+        private string TimeGreeting { get; set; }
+
+        /// <summary>
+        /// Sets the beginning of the greeting depending on current time
+        /// </summary>
+        private void SetGreetingFormat()
+        {
+            // current hour
+            int currentHour = DateTime.Today.Hour;
+
+            // set greeting based on time
+            if (Enumerable.Range(5, 12).Contains(currentHour))
+                TimeGreeting = "Good morning";
+            else if (Enumerable.Range(13, 16).Contains(currentHour))
+                TimeGreeting = "Good afternoon";
+            else
+                TimeGreeting = "Good evening";
+        }
+
+        /// <summary>
+        /// Sets greeting based on current time
+        /// </summary>
+        /// <param name="Name">User name</param>
+        /// <returns></returns>
+        public string Greeting(string Name)
+        {
+            SetGreetingFormat();
+            return $"{Greeting}, {Name}";
+        }
+    }
+}
