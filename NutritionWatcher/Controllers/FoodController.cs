7ea14f2c19255dc0bff76499756f80b9178bee86
PurpleAnteater <I.Kupchinenko@gmail.com>
2022-03-20 18:25:56 +0300

    Was created a food controller

---
+++ NutritionWatcher/Controllers/FoodController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NutritionWatcher.Models;
+using NutritionWatcher.ViewModels;
+using NutritionWatcher.Services;
+
+namespace NutritionWatcher.Controllers
+{
+    public class FoodController : Controller
+    {
+        private FoodFormModel foodFormModel { get; set; }
+
+        public FoodController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            foodFormModel = new FoodFormModel();
+            return View("FoodForm", foodFormModel);
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm(FoodFormModel foodForm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("FoodForm");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
