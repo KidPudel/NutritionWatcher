7817fdacd9f50a2c840acedd777814fedb5dfdcd
PurpleAnteater <I.Kupchinenko@gmail.com>
2022-03-20 18:25:16 +0300

    Was created a model for food form

---
+++ NutritionWatcher/Models/FoodFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NutritionWatcher.Models
+{
+    public class FoodFormModel
+    {
+        [Required (AllowEmptyStrings = false, ErrorMessage = "Please enter a food name")]
+        [Display(Name = "What you've eaten")]
+        public string FoodName { get; set; }
+
+        [Required (AllowEmptyStrings = false, ErrorMessage = "Please specify number of callories in 100g")]
+        [Display(Name = "How many callories in 100g")]
+        public int Callories { get; set; }
+
+        [Required (AllowEmptyStrings = false, ErrorMessage = "Please specify number of grams")]
+        [Display(Name = "Number of grams")]
+        public int Grams { get; set; } = 100;
+
+    }
+}
