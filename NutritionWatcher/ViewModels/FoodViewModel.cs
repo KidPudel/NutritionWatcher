41fb41e840b54e61717ac590fd67437d8253a7d0
PurpleAnteater <I.Kupchinenko@gmail.com>
2022-03-22 15:25:45 +0300

    Was corrected a name of FoodList propery

---
+++ NutritionWatcher/ViewModels/FoodViewModel.cs
@@ -0,0 +1,9 @@
+﻿using NutritionWatcher.Models;
+
+namespace NutritionWatcher.ViewModels
+{
+    public class FoodViewModel
+    {
+        public List<FoodModel> FoodList { get; set; } = new List<FoodModel>();
+    }
+}
